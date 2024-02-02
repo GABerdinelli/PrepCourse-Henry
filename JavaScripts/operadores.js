6699f6f1b6a41db84f4d7339dbcee696b667dcde
Ale GAB <alejandro.berdinelli@gmail.com>
2024-02-02 17:42:06 -0300

    Clase JavaScripts

---
+++ JavaScripts/operadores.js
@@ -0,0 +1,36 @@
+//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Resto
+console.log(95 % 4);
+
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8);
+
+
+console.log('HOLA ' + 'CAMILO');
+var nombre = 'CAMILO';
+var saludo = 'HOLA ' + nombre;
+console.log(saludo);
+
+
+console.log(3 + 4 * 5); // 3 + 20
+// Expected output: 23
+
+console.log(4 * 3 ** 2); // 4 * 9
+// Expected output: 36
+
+let a;
+let b;
+
+console.log((a = b = 5));
+// Expected output: 5
